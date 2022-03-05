2380f97a074c80bdb970814b5ebef77e196849ae
Carlos Alberto Silva Júnior <56141054+casjunior93@users.noreply.github.com>
2022-03-05 08:58:09 -0300

    banco criado no curso

---
+++ movies.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Tempo de geração: 05-Mar-2022 às 11:57
+-- Versão do servidor: 5.7.33
+-- versão do PHP: 7.4.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `movies-control`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `movies`
+--
+
+CREATE TABLE `movies` (
+  `id` int(11) NOT NULL,
+  `type` int(11) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `total_ep` int(11) DEFAULT NULL,
+  `atual_ep` int(11) DEFAULT NULL,
+  `last_view` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `movies`
+--
+
+INSERT INTO `movies` (`id`, `type`, `name`, `total_ep`, `atual_ep`, `last_view`) VALUES
+(1, 0, 'Friends', 10, 2, '2022-03-05 08:50:31'),
+(2, 1, 'Avengers', NULL, NULL, '2022-03-05 08:50:31'),
+(3, 1, 'Harry Potter', NULL, NULL, '2022-03-05 08:54:39');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `movies`
+--
+ALTER TABLE `movies`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `movies`
+--
+ALTER TABLE `movies`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
